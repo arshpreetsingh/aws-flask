e63779a9180d1ac3401f11d80a26c748e33fc0ee
Arshpreet Singh <arsh840@gmail.com>
2024-04-08 19:13:11 +0530

    Update instance_metadata.py

--- instance_metadata.py
+++ instance_metadata.py
@@ -1,5 +1,8 @@
+from flask import Flask, request, jsonify
 import requests
 import json
+
+app = Flask(__name__)
 
 def get_metadata(path):
     base_url = 'http://169.254.169.254/latest/meta-data/'
@@ -11,7 +14,6 @@ def get_metadata(path):
         return None
 
 def generate_json_output():
-    # Example paths - customize as needed
     data = {
         'instance-id': get_metadata('instance-id'),
         'instance-type': get_metadata('instance-type'),
@@ -21,6 +23,24 @@ def generate_json_output():
 
     return json.dumps(data, indent=2)
 
+def process_dict(json_object):
+    for key, value in json_object.items():
+        if isinstance(value, dict):
+            process_dict(value)
+        else:
+            return {'key':key,'value':value}
+
+
+@app.route('/')
+def api_endpoint():
+    data = generate_json_output()
+    return jsonify(data)
+
+@app.route('/process_data', methods=['POST'])
+def process_data_function():
+    request_data = request.get_json()
+    result = process_dict(request_data)
+    return jsonify(result)
+
 if __name__ == '__main__':
-    json_output = generate_json_output()
-    print(json_output)
+    app.run(host='0.0.0.0', port=5000)
